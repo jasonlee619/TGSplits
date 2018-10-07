571da78164969911793482b0818ac31cede37eb6
yli313 <yitong-li@uiowa.edu>
2018-10-07 17:53:57 -0500

    add TG class extended from Tourists

---
+++ src/People/TourGuide.java
@@ -0,0 +1,11 @@
+package People;
+
+public class TourGuide extends Tourists{
+    public static Integer year;
+    public void TourGuide(String name, String major, String home, Integer year){
+        this.name = name;
+        this.major = major;
+        this.home = home;
+        this.year = year;
+    }
+}
