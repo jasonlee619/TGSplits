c5d5acc07a4130411254ff07602cb04d78269154
yli313 <yitong-li@uiowa.edu>
2018-10-07 17:50:00 -0500

    create tourists class

---
+++ src/People/Tourists.java
@@ -0,0 +1,14 @@
+package People;
+
+public class Tourists {
+    public static String name;
+    public static String major;
+    public static String home;
+    public static boolean admit;
+    public void Tourists(String name, String major, String home, Boolean admit){
+        this.name = name;
+        this.major = major;
+        this.home = home;
+        this.admit = admit;
+    }
+}
